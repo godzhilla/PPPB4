bb03caa0584f3bbfa07c1bbda4f314341c51d0b8
godzhilla <ashilanajlasalsabillawd@mail.ugm.ac.id>
2023-09-12 12:24:23 +0700

    Tugas Constrain Layout

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PERTEMUAN 4_TUGAS CONSTRAIN LAYOUT"
+include(":app")
+ 
